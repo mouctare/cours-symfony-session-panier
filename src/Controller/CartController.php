64c7a52873c3a6ae7af24e96e1d17a86d056c8d7
mouctare <mouctard78@gmail.com>
2020-05-16 17:32:36 +0200

    Ajout de la session

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
@@ -13,5 +14,34 @@ class CartController extends AbstractController
     public function index()
     {
         return $this->render('cart/index.html.twig', []);
+    }
+    /**
+     * @Route("/panier/add/{id}", name="cart_add")
+     */
+    public function add($id, Request $request) { 
+        // La prémière de chose on crée une action!
+        $session = $request->getSession();
+
+        $panier = $session->get('panier', []);
+
+        if(!empty($panier[$id])) {
+            // Si j'ai déjà un produit qui a cet idifiant là
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+
+        }
+
+
+        $session->set('panier', $panier);
+
+        // Je prends c'est qu'il ya dans mon panier à l'époque je l'altère  en mettant c'est que j'ai de nouveau
+
+        dd($session->get('panier'));
+
+
+
+
+
     }
 }
